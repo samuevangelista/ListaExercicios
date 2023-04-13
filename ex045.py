082697e180f76c48ef452069ff631f8d09b95f47
Samuel Evangelista <samueo467@gmail.com>
2023-04-12 22:41:57 -0300

    12/04

--- ex045.py
+++ ex045.py
@@ -2,12 +2,40 @@ from random import randint
 from time import sleep
 n = randint(1,3)
 print('=== JOKENPÔ ===')
-escolha = int(input('-VAMOS JOGAR-\n'
+nn = int(input('-VAMOS JOGAR-\n'
                     '|PEDRA|   [1]\n'
                     '|PAPEL|   [2]\n'
                     '|TESOURA| [3]\n'
                     '(Escolha): '))
-print('PROCESSANDO...')
+print('==============\n '
+      'PROCESSANDO...\n'
+      '==============')
 sleep(3)
-if n == 1 and escolha == 1:
-    
+if n == nn:
+    if n == 1:
+        emp = 'PEDRA'
+    elif n == 2:
+        emp = 'PAPEL'
+    else:
+        emp = 'TESOURA'
+    print('{} X {}'.format(emp, emp))
+    print('EMPATE')
+else:
+    if n == 1 and nn == 2:
+        print('PEDRA X PAPEL')
+        print('VOCE GANHOU')
+    elif n == 1 and nn == 3:
+        print('PEDRA X TESOURA')
+        print('VOCE PERDEU')
+    elif n == 2 and nn == 1:
+        print('PAPEL X PEDRA')
+        print('VOCE PERDEU')
+    elif n == 2 and nn == 3:
+        print('PAPEL X TESOURA')
+        print('VOCE GANHOU')
+    elif n == 3 and nn == 1:
+        print('TESOURA X PEDRA')
+        print('VOCE GANHOU')
+    elif n == 3 and nn == 2:
+        print('TESOURA X PAPEL')
+        print('VOCE PERDEU')
