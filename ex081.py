b98c6e9ad31efbf472dd61e1e689161c4bd811e1
Samuel Evangelista <samueo467@gmail.com>
2023-11-06 07:41:35 -0300

    commit

--- ex081.py
+++ ex081.py
@@ -1,15 +1,13 @@
 lista = []
-cont = 0
 while True:
-    list.append(int(input('Digite um número: ')))
-    cont += 1
-
-    while resp not in 'SN':
-        resp = str(input('Quer continuar [S/N]: ')).strip().upper()
-    if resp == 'N':
+    lista.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
         break
+print('-=' * 30)
+print(f'Você digitou {len(lista)} elementos.')
 lista.sort(reverse=True)
-print(lista)
+print(f'Os valores em ordem decrescente são {lista}')
 if 5 in lista:
     print('O valor 5 está na lista')
 else:
